b352b40535895d56dd05d6b37a18a271d1f68249
withoutforget <89633210+withoutforget@users.noreply.github.com>
2023-04-20 15:03:36 +0300

    Add files via upload
    Removed variadic templates for private functions of Logger. 
    Added level_enum for level of logging, it will be done later. 
    Added define USE_MUTEX, for output with lock_guard.
    Added std::string_view m_filename as the private field. 
    Added void log(std::string_view filename, std::string_view text) for log in console and in file at the same time.
    Added bool log(std::string_view text) for log in console and in file at the same time, return true if m_filename is not empty, else return false without output, protected by local_mutex; 
    set_filename(std::string_view), get_filename(std::string_view&) and bool is_filename_empty().

--- Logger/logger.hpp
+++ Logger/logger.hpp
@@ -11,12 +11,26 @@
 
 
 
+#if defined(USE_MUTEX)
+std::mutex global_mutex;
+#endif
 
-std::mutex mutex;
+enum level_enum // to do: deal something with that
+{
+    off = -1,
+    critical = 0,
+    err = 1,
+    warn = 2,
+    info = 3,
+    debug = 4,
+    trace = 5
+};
 
 template<class ... Args>
 auto console_out = [](Args&& ... args) { 
-    //std::lock_guard<std::mutex> lk(mutex); // guess we don't need that
+#if defined(USE_MUTEX)
+    std::lock_guard<std::mutex> lk(global_mutex); // guess we don't need that in general 
+#endif
     fmt::print("[LOG][Date {0}] \"{1}\"\n",args...);
 };
 
@@ -30,22 +44,72 @@ auto file_out = [](std::string_view filename,Args&& ... args) {
 };
 
 class Logger {
-private:
-    template<class ... Args>
-    void console_out_(Args&& ... args) {
-        console_out<Args...>(std::forward<Args>(args)...);
+private: // for private fields
+    std::string_view m_filename;
+    std::mutex local_mutex;
+private: // for private funcs
+    template<class Date, class T>
+    void console_out_(Date&& date, T&& text) {
+        console_out<Date,T>(std::forward<Date>(date),std::forward<T>(text));
     }
-    template<class ... Args>
-    void file_out_(std::string_view filename, Args&& ... args) {
-        file_out<Args...>(filename,std::forward<Args>(args)...);
+    template<class Date, class T>
+    void file_out_(std::string_view filename, Date&& date, T&& text) {
+        file_out<Date,T>(filename,std::forward<Date>(date),std::forward<T>(text));
     }
-public: // TO DO: add normal date
-    void logc(std::string text) {
-        console_out_("",text);
+
+    auto get_datetime_() { // TO DO: make well enough datatime function
+        return "";
     }
-    void logf(std::string_view filename, std::string text) {
-        file_out_(filename,"", text);
+public:
+    void set_filename(std::string_view filename) {
+        std::lock_guard<std::mutex> lk(local_mutex);
+        m_filename = filename;
     }
+
+    bool get_filename(std::string_view& filename) {
+        std::lock_guard<std::mutex> lk(local_mutex);
+        if (m_filename.empty())
+            return false;
+            filename = m_filename;
+    }
+
+    bool is_filename_empty() {
+        std::lock_guard<std::mutex> lk(local_mutex);
+        return m_filename.empty();
+    }
+
+
+    void logc(std::string_view text) {
+        auto dt = get_datetime_();
+        console_out_(dt,text);
+    }
+    void logf(std::string_view filename, std::string_view text) {
+        auto dt = get_datetime_();
+        file_out_(filename,dt, text);
+    }
+
+    void log(std::string_view filename, std::string_view text) {
+        auto dt = get_datetime_();
+        file_out_(filename,dt,text);
+        console_out_(dt,text);
+    }
+
+    bool log(std::string_view text) {
+        std::string_view filename;
+        {
+            std::lock_guard<std::mutex> lk(local_mutex);
+            if (m_filename.empty()) // i hope that nobody won't use empty filename 
+                return false; // throw std::runtime_error("You are logging to file without filename.");
+            filename = m_filename; // i hope for call of copy assignment         
+        }
+        auto dt = get_datetime_();
+        file_out_(filename,dt,text);
+        console_out_(dt,text);
+        return true;
+    }
+
+
+
 };
 
 
