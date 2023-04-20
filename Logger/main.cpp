8b8361e16f158c4c123320581ae8425f46b4b18b
withoutforget <89633210+withoutforget@users.noreply.github.com>
2023-04-20 14:20:09 +0300

    Update main.cpp

--- Logger/main.cpp
+++ Logger/main.cpp
@@ -3,11 +3,9 @@
 #include <iostream>
 
 
-using namespace std::chrono_literals;
-
 void foo1() {
     Logger log;
-    for (size_t i = 0; i < 10000; i++) {
+    for (size_t i = 0; i < 1000000000; i++) {
         log.logc("Always is ok");
         log.logf("test.txt","Always is ok");
     }
@@ -15,7 +13,7 @@ void foo1() {
 
 void foo2() {
     Logger log;
-    for (size_t i = 0; i < 10000; i++) {
+    for (size_t i = 0; i < 1000000000; i++) {
         log.logc("Always is bad");
         log.logf("test.txt","Always is bad");
     }
@@ -27,46 +25,3 @@ int main() {
     std::thread t2(foo2);
     t1.join(); t2.join();
 }
-
-
-
-/*GlobalLogger logger;
-OutputFile* outfile = new OutputFile;
-
-
-auto printr = [](std::string str) -> int {  std::cout << str; return 0;};
-
-template<typename ... Args>
-auto print = [](Args ... args) {
-    (std::cout << ... << args);
-};
-
-
-void foo1() {
-    for(size_t i = 0; i < 5; i++) {
-        logger.log_console("Always is good\n",print<std::string>);
-            std::this_thread::sleep_for(50ms);
-    }
-}
-
-void foo2() {
-    for(size_t i = 0; i < 5; i++) {
-        logger.log_file("Always is bad\n","file.log",outfile);
-        std::this_thread::sleep_for(15ms);
-    }
-}
-
-
-int main() {
-    logger.set_file(outfile,"log.txt");
-    logger.set_console(printr);
-    std::cout << logger.is_console() << ' ' << logger.is_file() << std::endl;
-    std::this_thread::sleep_for(5ms);
-
-    std::thread t1(foo1);
-    std::thread t2(foo2);
-
-    t1.join();
-    t2.join();
-
-}*/
